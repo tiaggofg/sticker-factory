f1fdda04956969287875092ed41061546086889f
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-07-25 23:24:51 -0300

    Refactoring code and apply OOP

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,47 +1,23 @@
 package application;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.net.MalformedURLException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import extractor.ImdbExtractor;
+import httpClient.MyHttpClient;
 import imdb.ImdbMovie;
 
 public class Program {
 
-	public static void main(String[] args) throws IOException, InterruptedException, ParseException {
-		
-		String url = "https://alura-imdb-api.herokuapp.com/movies";
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		String body = response.body();
+	public static void main(String[] args) throws ParseException, MalformedURLException {
 
-		List<ImdbMovie> movies = new ArrayList<>();
-		JsonParser parser = new JsonParser();
-		List<Map<String, String>> data = parser.parse(body);
+		String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+		String json  = (new MyHttpClient()).getContent(url);
+		var extractor = new ImdbExtractor();
 		
-		for (Map<String, String> movie : data) {
-			String id = movie.get("id");
-			Integer rank = Integer.parseInt((movie.get("rank")));
-			String title = movie.get("title");
-			String fullTitle = movie.get("fullTitle");
-			URL imageUrl = new URL(movie.get("image"));
-			String[] crew = movie.get("crew").split(", ");
-			Date year = (new SimpleDateFormat("yyyy")).parse(movie.get("year"));
-			Float emdbRating = Float.parseFloat(movie.get("imDbRating"));
-			Float emdbRatingCount = Float.parseFloat(movie.get("imDbRatingCount"));	
-			movies.add(new ImdbMovie(id, rank, title, fullTitle, imageUrl, crew, year, emdbRating, emdbRatingCount));
-		}
+		List<ImdbMovie> movies = extractor.data(json);
+
 		
 	}
 
---
+++ src/extractor/ImdbExtractor.java
@@ -0,0 +1,44 @@
+package extractor;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import application.JsonParser;
+import imdb.ImdbMovie;
+
+public class ImdbExtractor {
+	
+	public List<ImdbMovie> data (String json) {
+		
+		try {
+			
+			List<Map<String, String>> data = (new JsonParser()).parse(json);
+			List<ImdbMovie> movies = new ArrayList<>();
+			
+			for (Map<String, String> movie : data) {
+				String id = movie.get("id");
+				Integer rank = Integer.parseInt((movie.get("rank")));
+				String title = movie.get("title");
+				String fullTitle = movie.get("fullTitle");
+				URL imageUrl = new URL(movie.get("image"));
+				String[] crew = movie.get("crew").split(", ");
+				Date year = (new SimpleDateFormat("yyyy")).parse(movie.get("year"));
+				Float emdbRating = Float.parseFloat(movie.get("imDbRating"));
+				Float emdbRatingCount = Float.parseFloat(movie.get("imDbRatingCount"));	
+				movies.add(new ImdbMovie(id, rank, title, fullTitle, imageUrl, crew, year, emdbRating, emdbRatingCount));
+			}
+			return movies;
+			
+		} catch(MalformedURLException | ParseException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+}
